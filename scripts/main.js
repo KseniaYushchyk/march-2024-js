90a57ae62b0d019d36801fd881b1da37e252e54c
Ksyha <ksenia.yushchyk.03@gmail.com>
2024-04-29 22:57:18 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+// 1
+let a_ = 'hello';
+let b_ = 'owu';
+let c_ = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a_);
+console.log(b_);
+console.log(c_);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+// 2
+let firstName = 'Ksenia';
+let middleName = 'Volodymyrivna';
+let lastname = 'Yushchyk';
+
+let PIB = firstName + ' ' + middleName + ' ' + lastname;
+console.log(PIB);
+
+//3
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, typeof b, typeof c);
+
+//4
+
+let firstNameOfUser = prompt("Введіть ваше ім'я:");
+let middleNameOfUser = prompt("Введіть ваше по-батькові:");
+let age = prompt("Введіть ваш вік:");
+
+console.log("Ваше ім'я: " + firstNameOfUser);
+console.log("Ваше по-батькові: " + middleNameOfUser);
+console.log("Ваше вік: " + age);
